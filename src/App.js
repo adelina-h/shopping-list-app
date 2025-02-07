5a96d173b942f1dad02fd26dbc06e53b9d7e3752
Adelina-hub <adelina.macarascu@gmail.com>
2025-02-07 17:44:28 +0200

    com

---
+++ src/App.js
@@ -0,0 +1,80 @@
+const shoppingItems = [
+  {
+    id: 1,
+    description: "milk",
+    quantity: 1,
+    bought: false,
+  },
+  {
+    id: 2,
+    description: "bread",
+    quantity: 2,
+    bought: true,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Title />
+      <Form />
+      <ShoppingList />
+      <Footer />
+    </div>
+  );
+}
+
+function Title() {
+  return <h1>🛍️ shopping list 🛍️</h1>;
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>What do you need to buy? 🛒</h3>
+      <select className="select">
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Type here..." />
+      <button className="btn">Add item</button>
+    </form>
+  );
+}
+
+function ShoppingList() {
+  return (
+    <div className="shopping-list">
+      <ul>
+        {shoppingItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.bought ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button className="btn-dlt">❌</button>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <em> 🛒 You have x items on your list and you already bought x (x%)</em>
+    </footer>
+  );
+}
